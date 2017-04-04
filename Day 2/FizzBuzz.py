723054e2158fd8eef92ecbb8f49cfa38146459ab
anthonyndunguwanja <ndunguanthony58@gmail.com>
2017-04-04 14:26:58 -0400

    Create FizzBuzz.py

---
+++ Day 2/FizzBuzz.py
@@ -0,0 +1,9 @@
+def fizz_buzz(number):
+    if (number % 3 == 0):
+      print("Fizz")
+    elif (number % 5 ==0):
+      print("Buzz")
+    elif (number % 3 == 0 and number % 5 == 0):
+      print("FizzBuzz")
+    else:
+      print(number)
