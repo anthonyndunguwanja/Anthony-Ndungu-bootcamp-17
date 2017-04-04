23148fe600b4f65a8064c44c2d888a07d7a48a2b
anthonyndunguwanja <ndunguanthony58@gmail.com>
2017-04-04 02:04:46 -0400

    Update prime_numbers.py

--- Day 1/prime_numbers.py
+++ Day 1/prime_numbers.py
@@ -1,1 +1,23 @@
 
+#Generating prime numbers 
+
+def gen_primenos(num):
+
+    my_nums = []
+    if num in (0, 1):
+        return "Zero or One cannot be prime numbers."
+
+    if num < 2:
+        return "Prime numbers cannot be less than two."
+
+    if type(num) != int:
+        return "Only integers are allowed."
+
+    for x in range(2, num + 1):
+        if x > 1:
+            for y in range(2, x):
+                if (x % y) == 0:
+                    break
+            else:
+                my_nums.append(x)
+    return my_nums
