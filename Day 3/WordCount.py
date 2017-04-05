cc266eff0597732b203a0c6024b628970c647ef3
anthonyndunguwanja <ndunguanthony58@gmail.com>
2017-04-05 09:16:09 -0400

    Create WordCount.py
    Word count lab

---
+++ Day 3/WordCount.py
@@ -0,0 +1,7 @@
+def words(sentence):#Sentence is an argument
+    my_string = sentence.lower().split()
+    dic = {} #Declare a dictionary
+    for word in my_string:
+        dic[word] = my_string.count(word) #Counting words in a dictionary
+    print(dic)#Output a dictionary
+  
