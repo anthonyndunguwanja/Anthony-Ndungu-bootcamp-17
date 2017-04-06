4e22c7fd1bda03702adbc094dc21c90e71e488e1
anthonyndunguwanja <ndunguanthony58@gmail.com>
2017-04-06 16:44:26 -0400

    Added http_client program

--- Day 3/http_client.py
+++ Day 3/http_client.py
@@ -1,1 +1,16 @@
+import json
+import urllib2
+
+# open the url and the screen name 
+# (The screen name is the screen name of the user for whom to return results for)
+def get_data():
+    url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=python"
+
+# this takes a python object and dumps it to a string which is a JSON
+# representation of that object
+    data = json.load(urllib2.urlopen(url))
+
+# print the result
+    print data
+get_data()
 
