9643d9581088261e3a86e61fa5530375cc154df5
anthonyndunguwanja <ndunguanthony58@gmail.com>
2017-04-07 10:07:35 -0400

    Modified prime generator program

--- Day 1/prime_numbers.py
+++ Day 1/prime_numbers.py
@@ -1,28 +1,25 @@
-
 #Generating prime numbers 
 
-def gen_primenos(num):
-    '''This is a function to output prime numbers using asymptotic analysis from 0 to n'''
+def prime_number_generator(number):
 
-    my_nums = []
-    if num in (0, 1):
-         '''Testing zero or one is not a prime number'''
+    my_numbers = []
+    if number in (0, 1):
         return "Zero or One cannot be prime numbers."
 
-    if num < 2:
-        '''Testing that a number less than two is not a prime mumber'''
+    if number < 2:
         return "Prime numbers cannot be less than two."
 
-    if type(num) != int:
+    if type(number) != int:
         return "Only integers are allowed."
 
-    for x in range(2, num + 1):
+    for x in range(2, number + 1):
         if x > 1:
             for y in range(2, x):
                 if (x % y) == 0:
                     break
             else:
-                my_nums.append(x)
-    return my_nums
-num = input("Enter a number to test whether it is a prime number:")
-print(gen_primenos(num))
+                my_numbers.append(x)
+    return my_numbers
+
+
+
