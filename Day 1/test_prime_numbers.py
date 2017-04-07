fa413fb1f1cf28e6aa0f8d2809a48998a8a0a8eb
anthonyndunguwanja <ndunguanthony58@gmail.com>
2017-04-07 10:25:42 -0400

    Updated prime numbers test

--- Day 1/test_prime_numbers.py
+++ Day 1/test_prime_numbers.py
@@ -1,27 +1,27 @@
 #Testing Prime Number Generator
 '''These are tests to be passed for a program to generate prime numbers'''
 import unittest
-from prime_numbers import gen_primenos
+from prime_numbers import prime_number_generator
 
 
 class PrimeGeneratorTest(unittest.TestCase):
     def test_invalid_type_string_list(self):
-        self.assertEqual(gen_primenos([]), "Integers only allowed")
+        self.assertEqual(prime_number_generator([]), "Integers only allowed")
     
     def test_only_positive(self):
-        self.assertEqual(gen_primenos(-1), "Prime numbers cannot be less than two.")
+        self.assertEqual(prime_number_generator(-1), "Prime numbers cannot be less than two.")
 
     def test_prime_numbers(self):
-        self.assertEqual(gen_primenos(10), [2, 3, 5, 7])
+        self.assertEqual(prime_number_generator(10), [2, 3, 5, 7])
 
     def test_one(self):
-        self.assertEqual(gen_primenos(1), "Zero or One cannot be prime numbers.")
+        self.assertEqual(prime_number_generator(1), "Zero or One cannot be prime numbers.")
 
     def test_invalid_type_string(self):
-        self.assertEqual(gen_primenos("String"), "Only integers are allowed.")
+        self.assertEqual(prime_number_generator("String"), "Only integers are allowed.")
 
     def test_zero(self):
-        self.assertEqual(gen_primenos(0), "Zero or One cannot be prime numbers.")
+        self.assertEqual(prime_number_generator(0), "Zero or One cannot be prime numbers.")
 
 if __name__ == "__main__":
     unittest.main()
